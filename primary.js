b441cee62e494d42fc4b5185931d4c75a108c900
brianlian38 <45692557+brianlian38@users.noreply.github.com>
2020-06-15 19:51:13 -0400

    Upload of Basic Website, sans projects
    The Basic website outline missing a few things as detailed in the ReadMe

--- primary.js
+++ primary.js
@@ -1,1 +1,45 @@
-// JavaScript source code
+// JavaScript Code
+
+
+
+//Check if jQuery is loaded
+window.onload = function () {
+    if (window.jQuery) {
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded   
+        alert("Doesn't Work");
+    }
+}
+
+//Reveal Text when element is hovered over
+//CURRENTLY UNUSED
+/*
+ *  <img src="placeholder.png" id="image" onMouseOver="showText('Some Text')" onMouseOut="hide();">
+ *  <div id="text"></div>
+*/
+function showText(text) {
+    document.getElementById("text").innerHTML = text;
+}
+function hide() {
+    document.getElementById("text").innerHTML = "";
+}
+
+
+//Colour Mode Switch (aka darkMode)
+$(document).ready(function () {
+    $(".toggle").click(function () {
+        $("body").toggleClass("dark-mode");
+        $(".block").toggleClass("red-mode");
+    });
+});
+
+
+/*
+$("#download").hover(function () {
+    $("#resume_img").addClass("rotate");
+}, function () {
+    $("#resume_img").removeClass("rotate");
+});
+*/
